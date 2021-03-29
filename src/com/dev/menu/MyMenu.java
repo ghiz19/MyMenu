6bf7740faf4a8da35607171dac14638f921373cc
ghiz19 <ghizlane123.el@gmail.com>
2021-03-29 18:15:37 +0200

    Second Commit

--- src/com/dev/menu/MyMenu.java
+++ src/com/dev/menu/MyMenu.java
@@ -1,5 +1,14 @@
 package com.dev.menu;
 
 public class MyMenu {
-
+			public static void main(String[] args) {
+				
+				System.out.println("heeeelllo");
+				/************/
+				/*dhdhdhhdhd
+				 * 
+				 * djsdjhsdjhdhdhdjhdh
+				 * dshhhhh
+				 */
+			}
 }
